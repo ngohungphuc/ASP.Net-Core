7e66456980924228118af244b8df9a6b5d7a21b4
ngohungphuc <ngohungphuc95@gmail.com>
2016-08-10 21:40:40 +0700

    Creating a Model Class

---
+++ src/AngularJS and MVC 6 Web API/Models/Quotes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngularJS_and_MVC_6_Web_API.Models
+{
+    public class Quotes
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+    }
+}
