f6fa127f579994c129090d221102cffa2ab39c4f
ngohungphuc <ngohungphuc95@gmail.com>
2016-08-13 16:10:07 +0700

    Creating the AngularJS Controller

---
+++ src/AspCore/scripts/quotesController.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('quotesController', quotesController);
+
+    quotesController.$inject = ['$scope', Quotes];
+
+    function quotesController($scope, Quotes) {
+        //calling Quotes Service
+        $scope.quotes = Quotes.query();
+    }
+})();
--- src/AspCore/wwwroot/_references.js
+++ src/AspCore/wwwroot/_references.js
@@ -2,3 +2,4 @@
 /// <reference path="../gruntfile.js" />
 /// <reference path="js/site.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
+/// <reference path="lib/jquery/dist/jquery.js" />
