459845117f0b952642393dd5245335142337e0ce
ngohungphuc <ngohungphuc95@gmail.com>
2016-08-14 21:38:35 +0700

    Specifying Client Side Routes

--- src/AspCore/scripts/app.js
+++ src/AspCore/scripts/app.js
@@ -3,11 +3,30 @@
 
     angular.module('myApp', [
         // Angular modules
-        //'ngRoute'
+        'ngRoute',
 
         // Custom modules
         'quotesService'
         // 3rd Party Modules
 
     ]);
+
+    angular.module('myApp').config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+        $routeProvider.when('/', {
+            templateUrl: 'partial/quotes.html',
+            controller: 'quotesController'
+        }).when('/quotes/add', {
+            templateUrl: 'partial/add.html',
+            controller: 'quotesAddController'
+        }).when('/quotes/edit/:id', {
+            templateUrl: 'partial/edit.html',
+            controller: 'quotesEditController'
+        }).when('/quotes/delete/:id', {
+            templateUrl: 'partial/delete.html',
+            controller: 'quotesDeleteController'
+        });
+
+        //friendly url
+        $locationProvider.html5Mode(true);
+    }]);
 })();
