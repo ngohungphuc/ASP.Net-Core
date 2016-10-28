311be43c22ffc6649cd6181c5f3e06f8ae16670a
ngohungphuc <ngohungphuc95@gmail.com>
2016-10-28 10:47:33 +0700

    Update another controller to test

---
+++ Project 2/OdeToFood/src/OdeToFood/Controllers/AboutController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Controllers
+{
+    [Route("about")]
+    public class AboutController
+    {
+        [Route("address")]
+        public string Address()
+        {
+            return "Address";
+        }
+    }
+}
