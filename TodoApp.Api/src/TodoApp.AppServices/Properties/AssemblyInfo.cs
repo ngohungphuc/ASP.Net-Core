4e822fefa216a3cc86752b52ff3ce1ba3d9054da
ngohungphuc <ngohungphuc95@gmail.com>
2017-02-08 20:37:28 +0700

    Setup structure for todo app

---
+++ TodoApp.Api/src/TodoApp.AppServices/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TodoApp.AppServices")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ebfdbb26-5166-4c90-af8b-cb78dbff2fb2")]
