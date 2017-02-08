d150d5e9b134d62d3c85b0085d5c954f8337e4b3
ngohungphuc <ngohungphuc95@gmail.com>
2017-02-08 23:43:13 +0700

    Add IoC config

---
+++ TodoApp.Api/src/TodoApp.IoC/IoCConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TodoApp.IoC
+{
+    public static class IoCConfiguration
+    {
+        public static void Configuration(IServiceCollection service)
+        {
+            Configuration(service, Data.IoC.Module.GetTypes());
+            Configuration(service, DomainServices.IoC.Module.GetTypes());
+            Configuration(service, AppServices.IoC.Module.GetTypes());
+        }
+
+        public static void Configuration(IServiceCollection service, Dictionary<Type, Type> types)
+        {
+            foreach (var type in types)
+            {
+                service.AddScoped(type.Key, type.Value);
+            }
+        }
+    }
+}
